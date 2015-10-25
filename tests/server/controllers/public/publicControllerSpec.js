80b18a59bfbc0d0fa828347b4777e7b81f70c88d
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2015-10-25 23:15:48 +0100

    added test for demo index page

--- tests/server/controllers/public/publicControllerSpec.js
+++ tests/server/controllers/public/publicControllerSpec.js
@@ -1,3 +1,14 @@
+const request = require('supertest');
+const app = require('../../../../server/app');
+
 describe('publicController', () => {
   'use strict';
+
+  it('GET /turing-microservice', (done) => {
+    request(app)
+      .get('/turing-microservice')
+      .expect(
+        '<html>\n<head>\n  <title>turing-microservice</title>\n</head>\n<body>\n<h1>turing-microservice</h1>\n</body>\n</html>')
+      .expect(200, done);
+  });
 });
