2232c8f20abbeee42926461cfa14e04bb42a62fb
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2015-10-26 13:37:12 +0100

    fixed test

--- tests/server/controllers/public/publicControllerSpec.js
+++ tests/server/controllers/public/publicControllerSpec.js
@@ -4,9 +4,9 @@ const app = require('../../../../server/app');
 describe('publicController', () => {
   'use strict';
 
-  it('GET /turing-microservice', (done) => {
+  it('GET /turing-microservice/', (done) => {
     request(app)
-      .get('/turing-microservice')
+      .get('/turing-microservice/')
       .expect(
         '<html>\n<head>\n  <title>turing-microservice</title>\n</head>\n<body>\n<h1>turing-microservice</h1>\n</body>\n</html>')
       .expect(200, done);
