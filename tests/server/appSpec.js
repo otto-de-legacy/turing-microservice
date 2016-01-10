167a92962363457e5331d5a05d1bc1257c5cc91e
Benedikt Stemmildt <benedikt@stemmildt.com>
2016-01-10 01:02:05 +0100

    fixed test

--- tests/server/appSpec.js
+++ tests/server/appSpec.js
@@ -3,9 +3,9 @@ const app = require('../../server/app');
 const httpStatusHelper = require('../../server/helper/httpStatusHelper');
 
 describe('appSpec', () => {
-  it('GET /turing-microservice/assets/img/turing.jpg', (done) => {
+  it('GET /turing-microservice/img/turing.jpg', (done) => {
     request(app)
-      .get('/turing-microservice/assets/img/turing.jpg')
+      .get('/turing-microservice/img/turing.jpg')
       .expect('Content-Type', 'image/jpeg')
       .expect(httpStatusHelper.OK, done);
   });
