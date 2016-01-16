999d0f078aa3fc4049ed051c7d14ebefbee45939
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-01-16 20:47:19 +0100

    switch eslint from gulp to npm

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,22 +1,8 @@
-import eslint from 'gulp-eslint';
 import gulp from 'gulp';
 import istanbul from 'gulp-istanbul';
 import {Server as KarmaServer} from 'karma';
 import mocha from 'gulp-mocha';
 import runSequence from 'run-sequence';
-
-gulp.task('eslint', () => {
-  const jsFiles = [
-    './**/*.js',
-    '!./node_modules/**/*.js',
-    '!./src/server/public/**/*.js',
-    '!./target/**/*.js'
-  ];
-  return gulp.src(jsFiles)
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
 
 gulp.task('testServer', () => {
   process.env.NODE_ENV = 'production';
@@ -46,5 +32,5 @@ gulp.task('testPublic', (done) => {
 });
 
 gulp.task('test', (callback) => {
-  runSequence('eslint', 'testServer', 'testPublic', callback);
+  runSequence('testServer', 'testPublic', callback);
 });
