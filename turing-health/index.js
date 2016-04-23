d8410cc0d5bb50eb2534c7dd7d738d995344c21c
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-04-23 18:12:34 +0200

    we wont need diskusage info

--- turing-health/index.js
+++ turing-health/index.js
@@ -3,7 +3,6 @@
 const router = require('express').Router();
 const config = require('turing-config');
 
-// TODO: Add diskusage information (look at edison health page for inspiration)
 router.get(config.get('turing:server:routes:internal') + config.get('turing:health:route'), (request, response) => {
   response.json({
     status: 'UP',
