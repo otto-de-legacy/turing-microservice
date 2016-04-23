27977be2fd50aab1d315f5a82c79ea862bf2eb42
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-04-23 20:36:21 +0200

    use template string

--- turing-health/index.js
+++ turing-health/index.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 const config = require('turing-config');
 
-router.get(config.get('turing:server:routes:internal') + config.get('turing:health:route'), (request, response) => {
+router.get(`${config.get('turing:server:routes:internal')}${config.get('turing:health:route')}`, (request, response) => {
   response.json({
     status: 'UP',
     application: {
