e1bc4799ee7d5850d7e25f085cee821853982759
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-05-09 08:58:27 +0200

    dependency update

--- turing-example/src/client/component/ProductList.jsx
+++ turing-example/src/client/component/ProductList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import $ from 'jquery';
-import Card from 'material-ui/lib/card/card';
-import CardHeader from 'material-ui/lib/card/card-header';
-import CardText from 'material-ui/lib/card/card-text';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 
 class ProductList extends React.Component {
 
