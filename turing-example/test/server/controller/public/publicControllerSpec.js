8b7fdedb608066c5ecb57fecda7d794671def612
Benedikt Stemmildt <benedikt.stemmildt@otto.de>
2016-12-05 11:01:34 +0100

    eslint update

--- turing-example/test/server/controller/public/publicControllerSpec.js
+++ turing-example/test/server/controller/public/publicControllerSpec.js
@@ -10,7 +10,7 @@ describe('publicController', () => {
     request(new TuringExampleApp())
       .get('/turing-example/')
       .expect(
-        '<html>\n<head>\n  <title>turing-example</title>\n  <link rel="stylesheet" href="/turing-example/css/main.css"/>\n</head>\n<body>\n<div id="app"></div>\n<script type="text/javascript" src="/turing-example/js/app.js"></script>\n</body>\n</html>')
+        '<html><head><title>turing-example</title><link rel="stylesheet" href="/turing-example/css/main.css"/></head><body><div id="app"></div><script type="text/javascript" src="/turing-example/js/app.js"></script></body></html>')
       .expect(HttpStatusHelper.OK, done);
   });
 });
