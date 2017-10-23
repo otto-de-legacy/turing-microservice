5061a1efeb7c4983e4890cd0d49db6366fccdb03
Benedikt Stemmildt <benedikt@stemmildt.com>
2017-10-23 19:08:10 +0200

    chore(turing-microservice): config commitlint

--- packages/turing-example/commitlint.config.js
+++ packages/turing-example/commitlint.config.js
@@ -1,5 +1,141 @@
+/* eslint-disable max-lines */
+
 'use strict';
 
 module.exports = {
-  extends: ['@commitlint/config-angular']
+  rules: {
+    'body-leading-blank': [
+      2,
+      'always'
+    ],
+    'body-max-length': [
+      2,
+      'always',
+      Infinity
+    ],
+    'body-min-length': [
+      2,
+      'always',
+      0
+    ],
+    'body-tense': [
+      2,
+      'always',
+      ['present-imperative']
+    ],
+    'footer-leading-blank': [
+      2,
+      'always'
+    ],
+    'footer-max-length': [
+      2,
+      'always',
+      Infinity
+    ],
+    'footer-min-length': [
+      2,
+      'always',
+      0
+    ],
+    'footer-tense': [
+      2,
+      'always',
+      ['present-imperative']
+    ],
+    'header-max-length': [
+      2,
+      'always',
+      72
+    ],
+    'header-min-length': [
+      2,
+      'always',
+      0
+    ],
+    lang: [
+      2,
+      'always',
+      'eng'
+    ],
+    'scope-case': [
+      2,
+      'always',
+      'lowerCase'
+    ],
+    'scope-max-length': [
+      2,
+      'always',
+      Infinity
+    ],
+    'scope-min-length': [
+      2,
+      'always',
+      0
+    ],
+    'subject-case': [
+      2,
+      'always',
+      'lower-case'
+    ],
+    'subject-empty': [
+      2,
+      'never'
+    ],
+    'subject-full-stop': [
+      2,
+      'never',
+      '.'
+    ],
+    'subject-max-length': [
+      2,
+      'always',
+      Infinity
+    ],
+    'subject-min-length': [
+      2,
+      'always',
+      0
+    ],
+    'subject-tense': [
+      2,
+      'always',
+      ['present-imperative']
+    ],
+    'type-case': [
+      2,
+      'always',
+      'lower-case'
+    ],
+    'type-empty': [
+      2,
+      'never'
+    ],
+    'type-max-length': [
+      2,
+      'always',
+      Infinity
+    ],
+    'type-min-length': [
+      2,
+      'always',
+      0
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  }
 };
